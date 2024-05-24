56955e54aae4d930f4265bcf846c1abdb59c7eb6
Teameh <1330668+teameh@users.noreply.github.com>
2024-05-24 23:21:52 +0200

    First setup tutorials

---
+++ Sources/MountebankSwift/Documentation.docc/Resources/code/npm_install.sh
@@ -0,0 +1,2 @@
+$ npm install -g mountebank
+# This will take some time
---
+++ Sources/MountebankSwift/Documentation.docc/Resources/code/start_mountebank.sh
@@ -0,0 +1,6 @@
+$ npm install -g mountebank
+# This will take some time
+
+$  mb start
+info: [mb:2525] mountebank v2.9.1 now taking orders - point your browser to http://localhost:2525/ for help
+
