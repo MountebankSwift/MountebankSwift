a80182118d6be89787e15930224bf073955a8ddd
mat1th <mat1th@users.noreply.github.com>
2025-01-17 10:04:50 +0100

    fix(ios): make linux build work;

--- Sources/MountebankSwift/Common/DateFormatter.swift
+++ Sources/MountebankSwift/Common/DateFormatter.swift
@@ -1,9 +1,33 @@
 import Foundation
 
-struct DateFormatter {
+struct DateFormatter: @unchecked Sendable {
     init() {}
 
     static let shared = DateFormatter()
+
+    #if os(Linux)
+    static let isoDateFormatter: ISO8601DateFormatter = {
+        let isoDateFormatter = ISO8601DateFormatter()
+        isoDateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        isoDateFormatter.formatOptions = [
+            .withFullDate,
+            .withFullTime,
+            .withDashSeparatorInDate,
+            .withFractionalSeconds,
+        ]
+
+        return isoDateFormatter
+    }()
+
+    func formatToDate(_ isoDateString: String) -> Date {
+        // swiftlint:disable:next force_unwrapping
+        Self.isoDateFormatter.date(from: isoDateString)!
+    }
+
+    func formatFromDate(_ date: Date) -> String {
+        Self.isoDateFormatter.string(from: date)
+    }
+    #else
 
     func formatToDate(_ isoDateString: String) throws -> Date {
         let format = Date.ISO8601FormatStyle(includingFractionalSeconds: true)
@@ -13,4 +37,5 @@ struct DateFormatter {
     func formatFromDate(_ date: Date) -> String {
         date.formatted(Date.ISO8601FormatStyle(includingFractionalSeconds: true).dateSeparator(.dash))
     }
+    #endif
 }
