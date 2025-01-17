5e4cd8b0c672730c935e372f42b81e51e8098800
mat1th <mat1th@users.noreply.github.com>
2025-01-17 18:35:01 +0000

    fix: make date formatter work on older iOS versions; (#56)

--- Sources/MountebankSwift/Common/DateFormatter.swift
+++ Sources/MountebankSwift/Common/DateFormatter.swift
@@ -5,7 +5,6 @@ struct DateFormatter: @unchecked Sendable {
 
     static let shared = DateFormatter()
 
-    #if os(Linux)
     static let isoDateFormatter: ISO8601DateFormatter = {
         let isoDateFormatter = ISO8601DateFormatter()
         isoDateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
@@ -19,23 +18,20 @@ struct DateFormatter: @unchecked Sendable {
         return isoDateFormatter
     }()
 
-    func formatToDate(_ isoDateString: String) -> Date {
-        // swiftlint:disable:next force_unwrapping
-        Self.isoDateFormatter.date(from: isoDateString)!
-    }
-
-    func formatFromDate(_ date: Date) -> String {
-        Self.isoDateFormatter.string(from: date)
-    }
-    #else
-
     func formatToDate(_ isoDateString: String) throws -> Date {
-        let format = Date.ISO8601FormatStyle(includingFractionalSeconds: true)
-        return try format.parse(isoDateString)
+        if #available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *) {
+            let format = Date.ISO8601FormatStyle(includingFractionalSeconds: true)
+            return try format.parse(isoDateString)
+        } else {
+            return Self.isoDateFormatter.date(from: isoDateString)!
+        }
     }
 
     func formatFromDate(_ date: Date) -> String {
-        date.formatted(Date.ISO8601FormatStyle(includingFractionalSeconds: true).dateSeparator(.dash))
+        if #available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *) {
+            return date.formatted(Date.ISO8601FormatStyle(includingFractionalSeconds: true).dateSeparator(.dash))
+        } else {
+            return Self.isoDateFormatter.string(from: date)
+        }
     }
-    #endif
 }
