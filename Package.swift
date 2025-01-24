27f5e066969081303d9a3508dd0f73feb3f4ed9e
mat1th <mat1th@users.noreply.github.com>
2025-01-24 16:47:00 +0000

    Resolve datarace issue and enable `StrictConcurrency` on swift 5.10; (#58)
    feat: resolve datarace issue and enable `StrictConcurrency`;

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -27,7 +27,7 @@ let package = Package(
             dependencies: [],
             swiftSettings: [
                 // Enable to validate if project is compatible with swift 6.0 Concurrency
-                // .enableExperimentalFeature("StrictConcurrency")
+                .enableExperimentalFeature("StrictConcurrency"),
             ]
         ),
         .testTarget(
--- Sources/MountebankSwift/Common/DateFormatter.swift
+++ Sources/MountebankSwift/Common/DateFormatter.swift
@@ -5,7 +5,7 @@ struct DateFormatter: @unchecked Sendable {
 
     static let shared = DateFormatter()
 
-    static let isoDateFormatter: ISO8601DateFormatter = {
+    nonisolated(unsafe) static let isoDateFormatter: ISO8601DateFormatter = {
         let isoDateFormatter = ISO8601DateFormatter()
         isoDateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
         isoDateFormatter.formatOptions = [
@@ -19,6 +19,10 @@ struct DateFormatter: @unchecked Sendable {
     }()
 
     func formatToDate(_ isoDateString: String) throws -> Date {
+        #if os(Linux)
+        // swiftlint:disable:next force_unwrapping
+        return Self.isoDateFormatter.date(from: isoDateString)!
+        #else
         if #available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *) {
             let format = Date.ISO8601FormatStyle(includingFractionalSeconds: true)
             return try format.parse(isoDateString)
@@ -26,13 +30,18 @@ struct DateFormatter: @unchecked Sendable {
             // swiftlint:disable:next force_unwrapping
             return Self.isoDateFormatter.date(from: isoDateString)!
         }
+        #endif
     }
 
     func formatFromDate(_ date: Date) -> String {
+        #if os(Linux)
+        return Self.isoDateFormatter.string(from: date)
+        #else
         if #available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *) {
             return date.formatted(Date.ISO8601FormatStyle(includingFractionalSeconds: true).dateSeparator(.dash))
         } else {
             return Self.isoDateFormatter.string(from: date)
         }
+        #endif
     }
 }
