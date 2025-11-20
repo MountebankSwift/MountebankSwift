e11e66ef1735cb1c0b40beb22fd8925b8d0470ab
mat1th <mat1th@users.noreply.github.com>
2025-11-20 20:22:04 +0100

    bump pacakges + fix test;

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.17.7"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.18.7"),
     ],
     targets: [
         .target(
